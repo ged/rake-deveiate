19ec0e25a72f21433d1c9b4916d635c2a4a270b6
Michael Granger <ged@FaerieMUD.org>
2024-04-24 11:19:42 -0700

    Update dependencies for newer Rubies

--- gem.deps.rb
+++ gem.deps.rb
@@ -3,17 +3,17 @@
 source 'https://rubygems.org'
 
 gem 'rake', '~> 13.0'
-gem 'rdoc', '~> 6.3'
-gem 'rspec', '~> 3.10'
-gem 'simplecov', '~> 0.21'
+gem 'rdoc', '~> 6.6'
+gem 'rspec', '~> 3.13'
+gem 'simplecov', '~> 0.22'
 gem 'hglib', '~> 0.11'
 gem 'tty-prompt', '~> 0.23'
 gem 'tty-editor', '~> 0.7'
 gem 'tty-table', '~> 0.12'
 gem 'pastel', '~> 0.8'
-gem 'git', '~> 1.18'
-gem 'net-scp', '~> 3.0'
-gem 'ed25519', '~> 1.2'
+gem 'git', '~> 1.19'
+gem 'net-scp', '~> 4.0'
+gem 'ed25519', '~> 1.3'
 gem 'bcrypt_pbkdf', '~> 1.1'
 
 group :development do
